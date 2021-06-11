d7175aa1e82c23c9924972fda978db5061e49cb4
Siohfox <72400408+Siohfox@users.noreply.github.com>
2021-06-11 16:39:19 +0100

    Set up colour class

--- RayTracer/Colour.cpp
+++ RayTracer/Colour.cpp
@@ -1,1 +1,18 @@
 #include "Colour.h"
+
+
+Colour::Colour()
+{
+	m_red = 0.5;
+	m_green = 0.5;
+	m_blue = 0.5;
+	m_special = 0.5;
+}
+
+Colour::Colour(double red, double green, double blue, double special)
+{
+	m_red = red;
+	m_green = green;
+	m_blue = blue;
+	m_special = special;
+}
--- RayTracer/Colour.h
+++ RayTracer/Colour.h
@@ -1,5 +1,30 @@
-#pragma once
+#ifndef _COLOUR_H
+#define _COLOUR_H
+
+#include "math.h"
+
 class Colour
 {
+public:
+	Colour();
+
+	Colour(double, double, double, double);
+
+	// Methods
+	double getColourRed() { return m_red; }
+	double getColourGreen() { return m_green; }
+	double getColourBlue() { return m_blue; }
+	double getColourSpecial() { return m_special; }
+
+	double setColourRed(double redValue) { m_red = redValue; }
+	double setColourGreen(double greenValue) { m_green = greenValue; }
+	double setColourBlue(double blueValue) { m_blue = blueValue; }
+	double setColourSpecial(double specialValue) { m_special = specialValue; }
+
+private:
+	double m_red, m_green, m_blue, m_special;
 };
+
+#endif
+
 
